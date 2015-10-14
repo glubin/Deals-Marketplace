17b592585b108c819b67509681319848d6afd027
glubin <greg@greglubin.com>
2015-10-14 09:41:08 -0500

    visual sign in and log in

--- index.php
+++ index.php
@@ -25,12 +25,34 @@
 			</div>
 			<div id="navbar" class="navbar-collapse collapse">
 				<ul class="nav navbar-nav">
-					</ul>
+				</ul>
 
-			 <ul class="nav navbar-nav navbar-right">
-			        <li class="active"><a href="#">Home</a></li>
-			        <li><a href="#about">FAQ</a></li>
+				<ul class="nav navbar-nav navbar-right">
+					<!-- <li class="active"><a href="#">Home</a></li> -->
+					<li><a href="#FAQ">FAQ</a></li>
 					<li><a href="#about">About</a></li>
+					<li class="dropdown" id="menuLogin">
+						<a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Log In</a>
+						<div class="dropdown-menu">
+							<form class="form" id="formLogin" class="formLogin"> 
+								<input class="LogInBreak" name="username" id="username" type="text" placeholder="Username"> 
+								<input name="password" id="password" type="password" placeholder="Password"><br>
+								<button type="button" class="btn btn-info enterLogin">Log In</button>
+							</form>
+						</div>
+					</li>
+					<li class="dropdown" id="menuLogin">
+						<a class="dropdown-toggle" href="#" data-toggle="dropdown" id="navLogin">Sign Up</a>
+						<div class="dropdown-menu">
+							<form class="form" id="formLogin" class="formLogin"> 
+								<input class="LogInBreak" name="username" id="username" type="text" placeholder="Username"> 
+								<input class="LogInBreak"name="email" id="email" type="text" placeholder="Email"><br>
+								<input name="password" id="password" type="password" placeholder="Password"><br>
+								<button type="button" class="btn btn-info enterLogin">Sign Up</button>
+								<h4 class="signInDisclaimer">* Currently, you must be a Wash U student.</h4>
+							</form>
+						</div>
+					</li>
 					<!-- <li class="dropdown">
 			          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
 			          <ul class="dropdown-menu">
@@ -40,31 +62,32 @@
 			            <li role="separator" class="divider"></li>
 			            <li><a href="#">Separated link</a></li>
 			          </ul>
-			        </li> -->
-			      </ul>
+			      </li> -->
+			  </ul>
 
 
-				</div><!-- nav coll.-->
+			</div><!-- nav coll.-->
+		</div>
+	</nav>
+
+	<div class="container">
+
+		<!-- Main component for a primary marketing message or call to action -->
+		<div class="jumbotron">
+			<div class="col-xs-7">
+				<!-- <div class="productName"><h2 class="box">Chelsea FC Flag</h2><h4 class="box"> | </h4><h4 class="box">glubin</h4></div> -->
+				<div class="productName"><h2 class="">Chelsea FC Flag</h2></div>
 			</div>
-		</nav>
 
-		<div class="container">
-
-			<!-- Main component for a primary marketing message or call to action -->
-			<div class="jumbotron">
-				<div class="col-xs-7">
-					<!-- <div class="productName"><h2 class="box">Chelsea FC Flag</h2><h4 class="box"> | </h4><h4 class="box">glubin</h4></div> -->
-					<div class="productName"><h2 class="">Chelsea FC Flag</h2></div>
-				</div>
-				
-				<div class="row">
-				  <div class="col-xs-7"><img class="productImage" src="img/chelsea.png" alt="Chelsea FC Flag"></div>
-				  <div class="col-xs-4"><h3><strong>Seller:</strong> <i> Wash U Student </i></h3></div>
-				  <div class="col-xs-4 productInfo"><h3><strong>Condition: </strong> <i>Mint</i></h3></div>
-				  <div class="col-xs-4 productInfo"><h3><strong>Suggested Price:</strong> <i>$20</i></h3></div>
-				  <div class="col-xs-4 productInfo"><h3><strong>Description:</strong> </h3> </div>
-				  <div class="col-xs-4 productDescription"><h4>This is a flag in mint condition. I had never even opened it before! I'm excited to get this off my hands because I recently stopped watching soccer</h4></div>
-				</div>
+			<div class="row">
+				<div class="col-xs-7"><img class="productImage" src="img/chelsea.png" alt="Chelsea FC Flag"></div>
+				<div class="col-xs-4"><h3><strong>Time Left:</strong> <i> 10:00 </i></h3></div>
+				<div class="col-xs-4 productInfo"><h3><strong>Seller:</strong> <i> Wash U Student </i></h3></div>
+				<div class="col-xs-4 productInfo"><h3><strong>Condition: </strong> <i>Mint</i></h3></div>
+				<div class="col-xs-4 productInfo"><h3><strong>Suggested Price:</strong> <i>$20</i></h3></div>
+				<div class="col-xs-4 productInfo"><h3><strong>Description:</strong> </h3> </div>
+				<div class="col-xs-4 productDescription"><h4>This is a flag in mint condition. I had never even opened it before! I'm excited to get this off my hands because I recently stopped watching soccer</h4></div>
+			</div>
 
 				<!-- <div class="row">
 					<div class="col-xs-7"><p>This is a flag in mint condition. I had never even opened it before!</p></div>
