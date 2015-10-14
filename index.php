1d94faeecdfa0740d2c98d5dd9a4738daeb061e3
glubin <greg@greglubin.com>
2015-10-14 07:50:26 -0500

    linking system;

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Deals Market</title>
+	<script src="js/bootstrap.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+ 	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	
+</body>
+</html>
