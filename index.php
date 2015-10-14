83906b69a3fb4f945cc8d2df513513592d82e2f8
glubin <greg@greglubin.com>
2015-10-14 08:33:40 -0500

    working pics

--- index.php
+++ index.php
@@ -53,6 +53,7 @@
 			<!-- Main component for a primary marketing message or call to action -->
 			<div class="jumbotron">
 				<div class="productName"><h2 class="">Chelsea FC Flag</h2><h4 class="box"> by </h4><h4 class="box">glubin</h4></div>
+				<img src="img/chelsea.png" alt="Chelsea FC Flag">
 				<p>This example is a quick exercise to illustrate how the default, static and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
 				<p>To see the difference between static and fixed top navbars, just scroll.</p>
 				<p>
